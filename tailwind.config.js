b53225852ca8cb4cd441a239b13b51578ebff86a
devesh0999 <devesh2cse@gmail.com>
2024-09-18 22:50:34 +0530

    added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#ee0181'
+      },
+      fontFamily: {
+        'display': ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
